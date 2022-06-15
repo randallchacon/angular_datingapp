88da037beef3e70bbe2c7b54cd58ada418dbffc1
randallchacon <info@randallchacon.com>
2022-06-14 23:05:31 -0600

    Creating Db using EF code first migrations and adding a new API controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;            
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }        
+    }
+}
