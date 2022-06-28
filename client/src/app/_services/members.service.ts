213c640ec9166ecaca6e79b1390a6a14083115c5
randallchacon <info@randallchacon.com>
2022-06-27 22:05:45 -0600

    Using an interceptor to send the token and routing to the detailed page

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
-import { Observable } from 'rxjs';
 
-const httpOptions = {
+/* const httpOptions = {
   headers: new HttpHeaders({    
     Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
-}
+} */
 
 @Injectable({
   providedIn: 'root'
@@ -19,10 +18,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
