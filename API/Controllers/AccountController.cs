cee476f58d4b9fc086c0e780808896022103a571
randallchacon <info@randallchacon.com>
2022-07-01 00:02:39 -0600

    Setting the main photo in the API and to the nav. Setting the main photo in the client. Finally, delete methodin the API and client

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,7 +51,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
 
             if(user == null) return Unauthorized("Invalid username");
 
@@ -66,7 +68,8 @@ namespace API.Controllers
             return new UserDTO
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
