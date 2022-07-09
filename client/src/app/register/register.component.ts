996153350f2cf860c9cf040b336a0f372ce8e759
randallchacon <info@randallchacon.com>
2022-07-09 11:45:51 -0600

    Expading the register form and adding a reusable date input

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -13,17 +13,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup;
+  maxDate: Date;
 
   constructor(private accountService: AccountService, private toastr: ToastrService,
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
   }
 
   initializeForm(){
     this.registerForm = this.fb.group({
+      gender: ['', Validators.required],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, 
         Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
