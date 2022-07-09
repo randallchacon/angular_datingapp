2bfbbf586a19d41734fe2571f825215b9a2fb14c
randallchacon <info@randallchacon.com>
2022-07-09 09:53:48 -0600

    Reactive forms, client side validation, adding custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,23 +12,44 @@ export class RegisterComponent implements OnInit {
   //@Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, 
+        Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    })
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true}
+    }
   }
 
   register(){
-    console.log(this.model);
-    this.accountService.register(this.model)
-    .subscribe({      
-      next: () => {this.cancel()},
-      error: error => { 
-        console.log(error.error); 
-        this.toastr.error(error.error);
-      }  
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model)
+    // .subscribe({      
+    //   next: () => {this.cancel()},
+    //   error: error => { 
+    //     console.log(error.error); 
+    //     this.toastr.error(error.error);
+    //   }  
+    // });
   }  
 
   cancel(){
