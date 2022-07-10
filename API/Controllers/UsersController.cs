5bcc9f679d48581a143114f5b2d13d525364bfd6
randallchacon <info@randallchacon.com>
2022-07-10 12:25:00 -0600

    Using the pagination classes and setting up client pagination

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -31,9 +32,13 @@ namespace API.Controllers
 
         [HttpGet]
         //[AllowAnonymous]
-        public async Task <ActionResult<IEnumerable<MemberDTO>>> GetUsers()
+        public async Task <ActionResult<IEnumerable<MemberDTO>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(users.CurrentPage, users.PageSize,
+                users.TotalCount, users.TotalPages);
+
             return Ok(users);
         }
 
